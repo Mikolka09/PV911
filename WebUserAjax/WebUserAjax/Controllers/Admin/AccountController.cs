ec6b8df23891c763f5d42ba115f3df29977f924b
Микола Матіос <mikolka09@gmail.com>
2021-08-26 17:41:46 +0300

    26.08.2021

---
+++ WebUserAjax/WebUserAjax/Controllers/Admin/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebUserAjax.Entities;
+
+namespace WebUserAjax.Controllers.Admin
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ProjectUser> _userManager;
+        private readonly SignInManager<ProjectUser> _signInManager;
+
+
+
+        public AccountController(UserManager<ProjectUser> userManager, SignInManager<ProjectUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            //Вывожу список пользователей
+            return View("~/Views/Admin/Account/index.cshtml", _userManager.Users);
+        }
+    }
+}
---
+++ WebUserAjax/WebUserAjax/Controllers/Admin/AdminDashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebUserAjax.Controllers.Admin
+{
+    public class AdminDashboardController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/index.cshtml");
+        }
+    }
+}
